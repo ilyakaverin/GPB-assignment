65381f0f19eed560219b7cc493f4e812facf56af
ikaverin <kaverin1990@gmail.com>
2022-07-04 22:28:56 +0300

    add more funcs to reducer

--- src/reducer.tsx
+++ src/reducer.tsx
@@ -1,15 +1,102 @@
-import { StoreInterface } from "./context/userContext"
+import { StoreInterface, NEW_LICENSE} from "./context/userContext"
+import { License } from "./models/licence";
+export type ACTIONTYPE = { type:string, datakey: string, payload?: any };
 
-export type ACTIONTYPE = 
-{ type: "username"; payload: string } | 
-{ type: "registryNumber"; payload: string } |
-{ type: "fullName"; payload: string } |
-{ type: "dateOfBirth"; payload: string } |
-{ type: "placeOfBirth"; payload: string } |
-{ type: "citizenship"; payload: string } |
-{ type: "snils"; payload: string } |
-{ type: "registerAdress"; payload: string } |
-{ type: "livingAdress"; payload: string }
+interface ACTIONMAP {
+    data: () => StoreInterface,
+    state: () => StoreInterface,
+    businessInfo: () => StoreInterface,
+    currentLicense: () => StoreInterface,
+    addLicense: () => StoreInterface,
+    clearLicenseForm: () => StoreInterface,
+    deleteLicense: () => StoreInterface,
+    editLicense: () => StoreInterface,
+    saveLicense: () => StoreInterface,
+    switchSurveyToggle: () => StoreInterface,
+    checkFilling: () => StoreInterface,
+    fillingInterrupted: () => StoreInterface,
+    checkToggles: () => StoreInterface,
+    modalToggle: () => StoreInterface,
+    prepareObject: () => StoreInterface
+}
 
-export const stateReducer = (state:StoreInterface, action:ACTIONTYPE) => ({ ...state, [action.type]: action.payload })
+
+export const stateReducer = (store:StoreInterface, action:ACTIONTYPE) => {
+
+    const { type, datakey, payload } = action
+
+    const actionMap:ACTIONMAP = {
+        data: () => ({ ...store, data: {...store.data, [datakey]: payload} }),
+        businessInfo: () => ({ ...store, businessInfo: {...store.businessInfo, [datakey]: payload} }),
+        state: () => ({...store, state: {...store.state, [datakey]: payload } }),
+        currentLicense: () => ({...store,  currentLicense: {...store.currentLicense, [datakey]: payload} }),
+        addLicense: () => {
+            const userLicense:License = new License(store.currentLicense);
+            const updatedStoreofLicenses = [...store.licenses, userLicense];
+
+            return {...store, [datakey]: updatedStoreofLicenses}
+        },
+        clearLicenseForm: () => {
+            const keys: string[] = Object.keys(store.currentLicense);
+            const copy: NEW_LICENSE = Object.assign({}, store.currentLicense);
+            keys.forEach((key) => copy[key as keyof NEW_LICENSE] = '' )
+            return {...store, currentLicense: copy}
+
+            
+        },
+        deleteLicense: () => {
+            const filteredLicenses = store.licenses.filter(license => license.id !== payload);
+            return {...store, licenses: filteredLicenses}
+        },
+        editLicense: () => {
+
+            const {id, ...rest} = payload as License;
+            const updatedStore = {...store, state:{...store.state, [datakey]: 'editing', currentEditingLicenseId: id}, currentLicense: rest };
+
+            return updatedStore
+        },
+        saveLicense: () => {
+             const { currentEditingLicenseId } = store.state;
+
+            const updatedLicenses = store.licenses.map(license => {
+                if(license.getId() === currentEditingLicenseId) {
+                    license.update(payload)
+                }
+                return license
+            })
+            return {...store, licenses: updatedLicenses}
+        },
+        switchSurveyToggle: () => ({...store, survey: {...store.survey,  [datakey]: payload} }),
+        checkFilling: () => {
+            //@ts-ignore
+            const isAllFieldsFilled = payload.every((key: string) => store[datakey][key].length > 0 ) ? 'approved' : 'filling'
+
+            return ({...store, state: {...store.state, fillState: {...store.state.fillState, [datakey]: isAllFieldsFilled} }})
+    
+        },
+        checkToggles: () => {
+
+            const isToggleSwiped = payload.some((value:boolean) => value === true) ? 'approved' : 'filling';
+            return ({...store, state: {...store.state, fillState: {...store.state.fillState, [datakey]: isToggleSwiped} }})
+        },
+        fillingInterrupted: () => {
+            return ({...store, state: {...store.state, fillState: {...store.state.fillState, [datakey]: 'invalid'} }})
+        },
+        modalToggle: () => {
+            return {...store, state: {...store.state, [datakey]: payload}}
+        },
+        prepareObject: () => {
+            const result = {
+                Common: store.data,
+                business: store.businessInfo,
+                licenses: store.state.hasLicense ? store.licenses : 'no licenses',
+                survey: store.survey
+            }
+    
+            return {...store, result}
+        }
+    }
+
+    return actionMap[type as keyof ACTIONMAP]()
+}
 
